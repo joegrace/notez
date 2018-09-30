85a2382cdd6dd0cebb15d9fc517bbe8fca1975d9
Joe Grace <joe@w1sk.com>
2018-09-29 20:17:06 -0400

    Now notes will auto save after 15 seconds if the save button has been pressed at least once on a new note, or if you are switching to an existing note.

--- resources/js/components/pages/HomeComponent.vue
+++ resources/js/components/pages/HomeComponent.vue
@@ -1,5 +1,6 @@
 <template src='./templates/home.html'></template><script>
     import NoteService from '../../services/NoteService'
+import { clearInterval } from 'timers';
     
     
     export default {
@@ -47,7 +48,7 @@
         methods: {
             editNote(note) {
                 this.currentNote = note
-                
+                this.currentNote.dirty = false
             },
             
             /*
@@ -63,14 +64,17 @@
             * This will save the current note
             */
             async saveNote(note) {
-                console.log('saving')
-                
                 let result = await NoteService.saveNote(note)
                 
                 if (result.constructor.name == 'Object') {
-                    console.log("Getting new notes");
-                    this.getAllNotes()
-                    this.currentNote = result;
+                    // Is this a new note? If so, get all notes.
+                    if (this.currentNote.id == undefined) {
+                        console.log(this.currentNote.id)
+                        this.getAllNotes()
+                        this.currentNote = result;
+                    }
+
+                    this.currentNote.dirty = false;
                 } else {
                     console.log("Save result not true: " + result);
                 }
@@ -113,11 +117,51 @@
                 let serviceResult = await NoteService.getAllNotes()
                 
                 this.notes = serviceResult
-            }
+            },
+            
         },
         
         async mounted() {
-            await this.getAllNotes()
+            let component = this
+            await component.getAllNotes()
+
+            // Check for dirty note and save every 5s.
+            component.$options.noteSaveInterval = setInterval(async () => {
+                // We only want to auto-save notes that have been saved
+                // at least once
+                if (component.currentNote.id == undefined)
+                    return
+
+                if (component.currentNote.dirty != undefined && component.currentNote.dirty === true) {
+                    // Form is dirty, save.
+                    console.log("Saving dirty note")
+                    await component.saveNote(component.currentNote)
+                    component.currentNote.dirty = false
+                } 
+            }, 15000)
+
+
+        },
+
+        /*
+         * Make sure to clear the interval when we change routes.
+         * I /think/ this is suppose to work. Gotta test this.
+         */
+        beforeDestroy() {
+            clearInterval(this.$options.noteSaveInterval)
+        },
+
+
+        watch: {
+            currentNote: {
+                handler(newVal, oldVal) {
+                    if (this.currentNote.dirty === false || this.currentNote.dirty == undefined) {
+
+                        this.currentNote.dirty = true
+                    }
+                },
+                deep: true
+            }
         }
     }
     
